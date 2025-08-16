df99c2144b389db0dae6003590cb8ad8255aeabf
Hyunkwanko <cjdshrwkd11@naver.com>
2025-08-17 00:45:47 +0900

    initial

---
+++ kakaotalk/src/test/java/com/autoever/kakaotalk/KakaotalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.autoever.kakaotalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaotalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
